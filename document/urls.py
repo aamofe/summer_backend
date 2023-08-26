22f1c12e7a31c439b1f855b481b2da168bf39c90
aamofe <aamofe@qq.com>
2023-08-26 15:36:09 +0800

    1

--- document/urls.py
+++ document/urls.py
@@ -7,6 +7,6 @@ from .views import *
 
 urlpatterns = [
     path('create_document/<int:team_id>/', views.create_document, name='create_document'),
-    path('view_document/<int:document_id>/', views.view_document, name='create_document'),
+    # path('view_document/<int:document_id>/', views.view_document, name='create_document'),
 
 ]
