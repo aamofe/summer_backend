1fd910dd1f30fffc75d386f6cca7d2b548864266
aamofe <aamofe@qq.com>
2023-08-29 23:58:40 +0800

    修改路由

--- document/urls.py
+++ document/urls.py
@@ -6,21 +6,19 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('create_document/<int:project_id>/', views.create_document, name='create_document'),
+    # path('create_document/<int:project_id>/', views.create_document, name='create_document'),
     path('view_document/<str:token>/', views.view_document, name='create_document'),
     path('share_document/',views.share_document),
     path('change_lock/',views.change_lock),
-    path('save_document/',views.save_document),
-    path('all_documents/<int:project_id>/',views.all_documents),
-    path('create_prototype/<int:project_id>/',views.create_prototype),
-    path('save_prototype/',views.save_prototype),
+    # path('save_document/',views.save_document),
+    # path('all_documents/<int:project_id>/',views.all_documents),
+    # path('create_prototype/<int:project_id>/',views.create_prototype),
+    # path('save_prototype/',views.save_prototype),
     path('view_prototype/',views.view_prototype),
-    path('all_prototype/<int:project_id>/',views.all_prototype),
-    path('all_deleted_document/',views.all_deleted_document),
-    path('restore_one_document/',views.restore_one_document),
-    path('recover_all_document/',views.recover_all_document),
-
-
+    # path('all_prototype/<int:project_id>/',views.all_prototype),
+    # path('all_deleted_document/',views.all_deleted_document),
+    # path('restore_one_document/',views.restore_one_document),
+    # path('recover_all_document/',views.recover_all_document),
     path('create/',views.create),
     path('delete/',views.delete),
     path('restore/',views.restore),
